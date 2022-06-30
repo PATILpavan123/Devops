e0a9d9ee7c928fa057617b470ad63492e0a45c33
pavan patil <89702712+PATILpavan123@users.noreply.github.com>
2022-06-30 22:28:45 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -48,3 +48,30 @@ mail -s 'Disk Space Alert' pavanpatil0744@gmail.com << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT% $MEMORY $CPU
 
 EOF
+
+-----------------------------------
+
+# vi /opt/scripts/memory-alert-1.sh
+
+#!/bin/bash
+ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}' | cut -d. -f1)
+
+if [ $ramusage -ge 45 ]; then
+SUBJECT="ATTENTION: Memory Utilization is High on $(hostname) at $(date)"
+MESSAGE="/tmp/Mail.out"
+TO="example@gmail.com"
+
+  echo "Memory Current Usage is: $ramusage%" >> $MESSAGE
+  echo "" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "Top Memory Consuming Processes Using top command" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "$(top -b -o +%MEM | head -n 20)" >> $MESSAGE
+  echo "" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "Top Memory Consuming Processes Using ps command" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "$(ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head)" >> $MESSAGE
+  mail -s "$SUBJECT" "$TO" < $MESSAGE
+  rm /tmp/Mail.out
+  fi
